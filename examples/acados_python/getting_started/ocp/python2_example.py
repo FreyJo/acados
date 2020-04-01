762539a654ee0a2950633f6afb863f7b9b2bbf35
Jonathan Frey <jonathanpaulfrey@gmail.com>
2020-04-01 20:25:53 +0200

    add python2 example

---
+++ examples/acados_python/getting_started/ocp/python2_example.py
@@ -0,0 +1,62 @@
+#
+# Copyright 2019 Gianluca Frison, Dimitris Kouzoupis, Robin Verschueren,
+# Andrea Zanelli, Niels van Duijkeren, Jonathan Frey, Tommaso Sartor,
+# Branimir Novoselnik, Rien Quirynen, Rezart Qelibari, Dang Doan,
+# Jonas Koenemann, Yutao Chen, Tobias Schöls, Jonas Schlagenhauf, Moritz Diehl
+#
+# This file is part of acados.
+#
+# The 2-Clause BSD License
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.;
+#
+
+# NOTE: this example can be run after running the minimal_example_ocp from Python3
+
+from acados_py2 import AcadosOcpSolver
+import numpy as np
+
+N = 20
+nx = 4
+nu = 1
+dummy_ocp = {'model': {"name":"pendulum_ode"}, 'solver_options': {"nlp_solver_type": "SQP"}}
+ocp_solver = AcadosOcpSolver(dummy_ocp, json_file = 'acados_ocp.json', use_compiled_solver = True)
+
+simX = np.ndarray((N+1, nx))
+simU = np.ndarray((N, nu))
+
+status = ocp_solver.solve()
+
+if status != 0:
+    raise Exception('acados returned status {}. Exiting.'.format(status))
+
+# get solution
+for i in range(N):
+    simX[i,:] = ocp_solver.get(i, "x")
+    simU[i,:] = ocp_solver.get(i, "u")
+simX[N,:] = ocp_solver.get(N, "x")
+
+print("simX", simX)
+print("simU", simU)
+
+ocp_solver.print_statistics() # encapsulates: stat = ocp_solver.get_stats("statistics")
