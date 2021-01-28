f2bb1ba49d0bba7384883c9a4ba48adc84363210
Jonathan Frey <jonathanpaulfrey@gmail.com>
2021-01-28 16:24:22 +0100

    simulink: modify advanced example

--- examples/acados_matlab_octave/getting_started/simulink_example_advanced.m
+++ examples/acados_matlab_octave/getting_started/simulink_example_advanced.m
@@ -12,8 +12,11 @@ simulink_opts = get_acados_simulink_opts;
 
 % manipulate simulink_opts to [de]activate in- & outputs (preliminary)
 % simulink_opts.outputs.sqp_iter = 0;
-simulink_opts.outputs.utraj = 1;
-simulink_opts.outputs.xtraj = 1;
+% simulink_opts.outputs.utraj = 1;
+% simulink_opts.outputs.xtraj = 1;
+
+simulink_opts.inputs.cost_W_0 = 1;
+
 ocp.generate_c_code(simulink_opts);
 
 %% Compile Sfunctions
@@ -26,10 +29,10 @@ make_sfun; % ocp solver
 %% Copy Simulink example blocks into c_generated_code
 source_folder = fullfile(pwd, '..');
 target_folder = pwd;
-copyfile( fullfile(source_folder, 'simulink_model_closed_loop.slx'), target_folder );
+copyfile( fullfile(source_folder, 'simulink_model_advanced_closed_loop.slx'), target_folder );
 
 %% Open Simulink example blocks
-open_system(fullfile(target_folder, 'simulink_model_closed_loop'))
+open_system(fullfile(target_folder, 'simulink_model_advanced_closed_loop'))
 
 % copyfile( fullfile(source_folder, 'simulink_model_integrator.slx'), target_folder );
 % open_system(fullfile(target_folder, 'simulink_model_integrator'))
