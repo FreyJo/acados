b4702e16b6949135057536a78af8948caa459e6f
Jonathan Frey <jonathanpaulfrey@gmail.com>
2024-04-10 15:06:12 +0200

    add tmp_nvninx_np to workspace

--- acados/ocp_nlp/ocp_nlp_common.c
+++ acados/ocp_nlp/ocp_nlp_common.c
@@ -1740,6 +1740,15 @@ acados_size_t ocp_nlp_workspace_calculate_size(ocp_nlp_config *config, ocp_nlp_d
 
     size += 1 * blasfeo_memsize_dvec(np_max); //  tmp_nparam;
 
+    if (opts->with_solution_sens_wrt_params)
+    {
+        size += (N+1)*sizeof(struct blasfeo_dmat); // tmp_nvninx_np
+        for (int i = 0; i <= N; i++)
+        {
+            size += blasfeo_memsize_dmat(nv[i]+ni[i]+nx[i], np[i]);  // tmp_nvninx_np
+        }
+    }
+
     // array of pointers
     // cost
     size += (N+1)*sizeof(void *);
@@ -1912,8 +1921,22 @@ ocp_nlp_workspace *ocp_nlp_workspace_assign(ocp_nlp_config *config, ocp_nlp_dims
     c_ptr += ocp_nlp_out_calculate_size(config, dims);
 
     assign_and_advance_double(nv_max, &work->tmp_nv_double, &c_ptr);
+    if (opts->with_solution_sens_wrt_params)
+    {
+        assign_and_advance_blasfeo_dmat_structs(N + 1, &work->tmp_nvninx_np, &c_ptr);
+    }
+
     // align for blasfeo mem
     align_char_to(64, &c_ptr);
+
+    // blasfeo_dmat
+    if (opts->with_solution_sens_wrt_params)
+    {
+        for (int i = 0; i <= N; i++)
+        {
+            assign_and_advance_blasfeo_dmat_mem(nv[i]+ni[i]+nx[i], np[i], work->tmp_nvninx_np+i, &c_ptr);
+        }
+    }
 
     // blasfeo_dvec
     assign_and_advance_blasfeo_dvec_mem(nv_max, &work->tmp_nv, &c_ptr);
--- acados/ocp_nlp/ocp_nlp_common.h
+++ acados/ocp_nlp/ocp_nlp_common.h
@@ -411,6 +411,7 @@ typedef struct ocp_nlp_workspace
     struct blasfeo_dvec dxnext_dy;
     // optimal value gradient wrt params
     struct blasfeo_dvec tmp_np;
+    struct blasfeo_dmat *tmp_nvninx_np;
     // AS-RTI
     double *tmp_nv_double;
 
